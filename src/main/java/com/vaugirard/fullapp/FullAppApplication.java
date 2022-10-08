2642a482663f23d1e679d1dbc4f9f5c5a6f5ef93
martinlebosspopo <martindufour@free.fr>
2022-10-08 18:28:41 +0200

    First sync with jenkins

--- src/main/java/com/vaugirard/fullapp/FullAppApplication.java
+++ src/main/java/com/vaugirard/fullapp/FullAppApplication.java
@@ -17,6 +17,6 @@ public class FullAppApplication {
 
     @GetMapping
     public ResponseEntity<String> hello() {
-        return new ResponseEntity<>("Hello World !", HttpStatus.OK);
+        return new ResponseEntity<>("Hello Jenkins !", HttpStatus.OK);
     }
 }

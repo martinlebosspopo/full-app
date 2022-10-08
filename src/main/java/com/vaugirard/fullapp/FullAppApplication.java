d64b226d034aa340b34558f4e74d470278e9988e
i538262 <martin.dufour@sap.com>
2022-10-08 17:03:04 +0200

    Gradle Init

---
+++ src/main/java/com/vaugirard/fullapp/FullAppApplication.java
@@ -0,0 +1,13 @@
+package com.vaugirard.fullapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullAppApplication.class, args);
+    }
+
+}
